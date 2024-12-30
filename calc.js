4d76c5d7aaf45912a9f78ef69041ba59d1e8be74
Eric Pratt <zealot2323@gmail.com>
2024-12-30 20:22:05 +0100

    Added functionality to make the basic buttons work and update the screen state

--- calc.js
+++ calc.js
@@ -1,3 +1,13 @@
+let sum = 0;
+let currentNum = '';
+let currentOp = '';
+let previousNum = 0;
+
+const screen = document.querySelector('#screen');
+screen.textContent = sum;
+
+const buttons = document.querySelector('#buttons');
+
 const add = function(a, b) {
 	return a + b
 };
@@ -12,7 +22,7 @@ const multiply = function(a, b) {
 
 const divide = function(a, b) {
     return a / b
-  };
+};
 
 const operate = function(op, a, b) {
     switch(op) {
@@ -25,7 +35,59 @@ const operate = function(op, a, b) {
         case 'divide': 
             return divide(a, b);
     }
+};
+
+const updateScreen = function(num, op) {
+    if (op === 'equals') {
+        screen.textContent = sum;
+    } else {
+        screen.textContent = num;
+    }
 }
 
-console.log(operate('divide', 1, 2));
+buttons.addEventListener('click', (event) => {
+    let target = event.target;
 
+    switch(target.className) {
+        case 'num':
+            currentNum += target.textContent;
+            updateScreen(currentNum);
+            break;
+        case 'operator':
+            if (target.id === 'equals') {
+                currentNum = Number(currentNum);
+                sum = operate(currentOp, previousNum, currentNum);
+                updateScreen(sum, 'equals');
+                currentOp = '';
+                currentNum = '';
+            } else {
+                currentOp = target.id;
+                previousNum = Number(currentNum);
+                currentNum = '';
+
+                //update button state to pressed
+            };
+            break;
+        case 'misc':
+            switch(target.id) {
+                case 'clear':
+                    currentNum = '';
+                    currentOp = '';
+                    updateScreen('0');
+                    break;
+                case 'plusminus':
+                    if (currentNum.startsWith('-')) {
+                        currentNum = currentNum.substring(1);
+                    } else {
+                        currentNum = `-${currentNum}`;
+                    }
+                    updateScreen(currentNum);
+                    break;
+                case 'percentage':
+                    // move decimal place two spaces, 1 to .01
+                    //update screen
+                    break;
+            }
+            break;
+    }
+});
